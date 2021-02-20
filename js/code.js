916d1fc11bb1d955e5d2fb8dc334bd171e52f8df
Rishi Soni <rishisoni6071@gmail.com>
2021-02-21 02:15:30 +0530

    Static Work Completed

--- js/code.js
+++
@@ -1,5 +0,0 @@
-// Dynamically Rendering the Add Note Form
-document.body.addEventListener('DOMContentLoaded', () => {
-
-    const addNote_box = document.querySelector('#addNote_box');
-});
---
+++ js/index.js
@@ -0,0 +1,4 @@
+// Calls by Clicking Add Note
+document.querySelector('#addnote_btn').addEventListener('click', (event) => {
+    event.preventDefault();
+});
